535f848b2bd227e7f2f0e012846d970293be28d1
Glummy <58010838+Glumboi@users.noreply.github.com>
2026-06-10 11:43:57 +0200

    Fix broken unitypatcher download

--- src/EZModInstallerRemake/UnityPatcher/UnityPatcherInstaller.cs
+++ src/EZModInstallerRemake/UnityPatcher/UnityPatcherInstaller.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
@@ -17,59 +18,56 @@ namespace EZModInstallerRemake.UnityPatcher
         private readonly FileZipper _fileZipper = new FileZipper();
         private string _pcbsfolder;
         private string _tempDlPath;
-        private readonly string _unityPatcher = @"\unitypatcher";
+        private string _installPath; 
+        private const string  UNITYPATCHER_FOLDER = "\\unitypatcher";
         private bool _downloadDone = false;
-
+        private const string UNITYPATCHER_DONWLOAD_URL = "https://github.com/Glumboi/unitypatcher/releases/download/0.0.5/unitypatcher.zip";
+        
         public bool DownloadDone => _downloadDone;
 
         public void InstallUnitypatcher(string pcbsfolder)
         {
             _pcbsfolder = pcbsfolder;
-
-            string unityPatcherURL = "https://anonfiles.com/2bw9bcg1z7/unitypatcher_zip";
-
+            _installPath =$"{_pcbsfolder}{UNITYPATCHER_FOLDER}";
+            
             //Check if already isntalled
-            if (Directory.Exists(_pcbsfolder + _unityPatcher))
+            if (Directory.Exists(_installPath))
             {
                 DialogResult dialogResult = MessageBox.Show("unitypatcher is already installed!\nDo you want to reinstall it?", "info", MessageBoxButtons.YesNo,
                   MessageBoxIcon.Information);
 
                 if (dialogResult == DialogResult.Yes)
                 {
-                    Directory.Delete(pcbsfolder + _unityPatcher, true);
-                    StartDownload(unityPatcherURL);
+                    Directory.Delete(_installPath, true);
+                    StartDownload(UNITYPATCHER_DONWLOAD_URL);
                 }
 
                 return;
             }
-            StartDownload(unityPatcherURL);
+            StartDownload(UNITYPATCHER_DONWLOAD_URL);
         }
 
+        
+        
         private void StartDownload(string url)
         {
             _downloadDone = false;
-
-            using (AnonFileWrapper afwAnonFileWrapper = new AnonFileWrapper())
+            using (WebClient wbc = new WebClient())
             {
-                string ddl =
-                    afwAnonFileWrapper.
-                    GetDirectDownloadLinkFromLink(url);
-
-                using (WebClient wbc = new WebClient())
+                wbc.DownloadFileCompleted += Wbc_DownloadFileCompleted;
+                wbc.DownloadProgressChanged += (sender, args) =>
                 {
-                    wbc.DownloadFileCompleted += Wbc_DownloadFileCompleted;
-
-                    //Set path of Download
-                    _tempDlPath = _pcbsfolder + "TempDL.zip";
-
-                    wbc.DownloadFileAsync(new Uri(ddl), _tempDlPath);
-                }
+                    Debug.WriteLine($"Downloading: {args.ProgressPercentage}%");
+                };
+                _tempDlPath = _pcbsfolder + "unitypatcher.temp.zip";
+                wbc.DownloadFileAsync(new Uri(url), _tempDlPath);
             }
         }
 
         private void Wbc_DownloadFileCompleted(object sender, System.ComponentModel.AsyncCompletedEventArgs e)
         {
-            _fileZipper.Unzip(_tempDlPath, _pcbsfolder);
+            if(!Directory.Exists(_installPath)) Directory.CreateDirectory(_installPath);
+            _fileZipper.Unzip(_tempDlPath, _installPath);
             _downloadDone = true;
         }
     }
